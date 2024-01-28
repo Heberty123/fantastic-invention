ce443222bdabc9d5fba0e43748afabee457e074a
heberty <heberty.prado@mercadolivre.com>
2024-01-28 18:54:31 -0300

    just test again

--- src/main/java/br/com/Loja/LojaApplication.java
+++ src/main/java/br/com/Loja/LojaApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LojaApplication {
 
+	//teste
 	public static void main(String[] args) {
 		SpringApplication.run(LojaApplication.class, args);
 	}
