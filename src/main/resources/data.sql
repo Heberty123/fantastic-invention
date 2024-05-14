a453bdbe6c8d7d01c7c0808c5978b303d6e5eb14
heberty <hebertyprado22005@gmail.com>
2024-05-13 21:04:05 -0300

    updated

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,16 @@
+INSERT INTO customer (name, cpf) VALUES ('Heberty Guelfi do Pradp', '928.235.237-11');
+INSERT INTO customer (name, cpf) VALUES ('Amélia Guelfi do Prado', '837.237.823-82');
+INSERT INTO customer (name, cpf) VALUES ('Donizeti do Prado', '234.726.235-87');
+INSERT INTO customer (name, cpf) VALUES ('Andreia Guelfi dos Santos', '839.459.344-22');
+INSERT INTO customer (name, cpf) VALUES ('Alice dos Santos', '238.736.223-33');
+INSERT INTO customer (name, cpf) VALUES ('Aracelio francisco dos santos', '478.364.233-54');
+INSERT INTO customer (name, cpf) VALUES ('Maria de Lucia Guelfi', '236.239.234-23');
+
+
+INSERT INTO delivery_type (name) VALUES ('RESIDENCIAL');
+INSERT INTO delivery_type (name) VALUES ('CONDOMÍNIO');
+INSERT INTO delivery_type (name) VALUES ('TRABALHO');
+
 INSERT INTO payment_type (name) VALUES ('CRÉDTIO');
 INSERT INTO payment_type (name) VALUES ('DÉBITO');
 INSERT INTO payment_type (name) VALUES ('PIX');
